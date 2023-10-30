d1f5c613dcbb85f610be832bf6d52dc118e3d4cc
Vitalii Seniuk <vitaliiseniuk04@gmail.com>
2023-10-30 19:52:18 +0200

    Configure rabbitMQ

--- src/main/java/org/brain/storageservice/config/RabbitMQConfiguration.java
+++ src/main/java/org/brain/storageservice/config/RabbitMQConfiguration.java
@@ -2,7 +2,7 @@ package org.brain.storageservice.config;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -13,35 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class RabbitMQConfiguration {
-    private final RabbitQueueProperties queueProperties;
 
-
-    /**
-     * spring bean to create rabbitmq queue
-     **/
-    @Bean
-    public Queue queue() {
-        return new Queue(queueProperties.moveQueueName(), true);
-    }
-
-    /**
-     * define exchange
-     **/
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(queueProperties.exchangeName());
-    }
-
-    /**
-     * bind queue to exchange
-     **/
-    @Bean
-    public Binding binding() {
-        return BindingBuilder
-                .bind(queue())
-                .to(exchange())
-                .with(queueProperties.routingKey());
-    }
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
