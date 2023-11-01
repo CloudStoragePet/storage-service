0952c101e282e8efc443a38af5a8aa58a100897c
Vitalii Seniuk <vitaliiseniuk04@gmail.com>
2023-11-01 16:27:58 +0200

    fix redis connection

--- src/main/java/org/brain/storageservice/config/RedisConfig.java
+++ src/main/java/org/brain/storageservice/config/RedisConfig.java
@@ -3,32 +3,39 @@ package org.brain.storageservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-//@EnableJpaRepositories(basePackages = "org.brain.storageservice.repository")
+@EnableRedisRepositories(basePackages = "org.brain.storageservice.repository")
 public class RedisConfig {
 
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
+    //    @Bean
+//    JedisConnectionFactory jedisConnectionFactory() {  // default redis connection
+//        return new JedisConnectionFactory();
+//    }
 //    @Bean
 //    public JedisConnectionFactory jedisConnectionFactory() { // configure custom redis connection
 //        JedisConnectionFactory jedisConFactory
 //                = new JedisConnectionFactory();
-//        jedisConFactory.setHostName("localhost");
-//        jedisConFactory.setPort(6379);
+//        jedisConFactory.setHostName(host);
+//        jedisConFactory.setPort(port);
 //        return jedisConFactory;
 //    }
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) { // for custom repository
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory jedisConnectionFactory) { // for custom repository
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+        template.setConnectionFactory(jedisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer()); // string to byte array serializer
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
         return template;
     }
-
 
 
 }
